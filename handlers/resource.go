26a1656352c5d97682cd853498560b72285e6aed
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-02-13 17:16:44 +0900

    Refactor

--- handlers/resource.go
+++ handlers/resource.go
@@ -1,4 +1,4 @@
-package resource
+package handlers
 
 import "github.com/gin-gonic/gin"
 
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"AltTube-Go/handlers/piped"
 	"AltTube-Go/handlers/piped/opensearch"
 	"AltTube-Go/handlers/pipedproxy"
-	"AltTube-Go/handlers/resource"
 	"github.com/gin-gonic/gin"
 	"log"
 	"os"
@@ -29,7 +28,7 @@ func startApi() {
 	r.GET("/pipedproxy/*action", pipedproxy.PipedProxy)
 	r.GET("/resource", gin.BasicAuth(gin.Accounts{
 		"admin": "secret",
-	}), resource.Resource)
+	}), handlers.Resource)
 	if err := r.Run(":" + os.Getenv("PORT")); err != nil {
 		log.Fatalf("API failed to start: %v", err)
 	}
