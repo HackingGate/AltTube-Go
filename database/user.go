288a8c9f29840e2c7d57fc25882c613d099f1a03
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-02-14 23:35:04 +0900

    Check if the user exists when add and delete user

--- database/user.go
+++ database/user.go
@@ -1,10 +1,26 @@
 package database
 
-import "AltTube-Go/models"
+import (
+	"AltTube-Go/models"
+	"errors"
+	"gorm.io/gorm"
+)
 
 func AddUser(user models.User) error {
-	dbInstance.Create(&user)
-	return nil
+	// Check if the user already exists
+	existingUser, err := GetUserByEmail(user.Email)
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		// An error occurred that isn't the record not found error
+		return err
+	}
+	if existingUser != nil {
+		// User already exists
+		return errors.New("user already exists")
+	}
+
+	// If user does not exist, create the user
+	dbResult := dbInstance.Create(&user)
+	return dbResult.Error
 }
 
 func GetUserByEmail(email string) (*models.User, error) {
@@ -18,8 +34,20 @@ func GetUserByEmail(email string) (*models.User, error) {
 }
 
 func UpdateUserByEmail(email string, user models.User) error {
-	// Update user by email
-	dbResult := dbInstance.Model(&models.User{}).Where("email = ?", email).Updates(user)
+	// Check if the user exists
+	var existingUser models.User
+	dbResult := dbInstance.Where("email = ?", email).First(&existingUser)
+	if dbResult.Error != nil {
+		if dbResult.Error == gorm.ErrRecordNotFound {
+			// User does not exist
+			return errors.New("user not found")
+		}
+		// Other error
+		return dbResult.Error
+	}
+
+	// Perform the update
+	dbResult = dbInstance.Model(&existingUser).Updates(user)
 	if dbResult.Error != nil {
 		return dbResult.Error
 	}
