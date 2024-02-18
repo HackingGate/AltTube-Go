940f2759f93efec4534f5cab90987366a6c6e0a8
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-02-18 23:23:02 +0900

    Change GetRefreshTokenByUserID's order

--- database/get_refreshtoken_by_userid.go
+++ database/get_refreshtoken_by_userid.go
@@ -4,7 +4,7 @@ import "AltTube-Go/models"
 
 func GetRefreshTokenByUserID(userID string) ([]models.RefreshToken, error) {
 	var result []models.RefreshToken
-	dbResult := dbInstance.Where("user_id = ?", userID).Find(&result)
+	dbResult := dbInstance.Where("user_id = ?", userID).Order("created_at desc").Find(&result)
 	if dbResult.Error != nil {
 		return nil, dbResult.Error
 	}
