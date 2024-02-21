82a834bd2cbc21c65413e83fcc15758912d9d9bf
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-02-21 21:40:30 +0900

    Refactor

--- models/token.go
+++ models/token.go
@@ -14,3 +14,13 @@ type AccessToken struct {
 	RefreshTokenID uint         `gorm:"index"`
 	RefreshToken   RefreshToken `gorm:"foreignKey:RefreshTokenID"`
 }
+
+type RefreshToken struct {
+	gorm.Model
+	Token     string `gorm:"index:idx_token"`
+	Expiry    time.Time
+	UserAgent string
+	IPAddress string
+	UserID    string `gorm:"index"`
+	User      User   `gorm:"foreignKey:UserID"`
+}
