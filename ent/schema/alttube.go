9330938ec743628bfce1f2d55a18d8e8f294c0ce
HackingGate <8541644+HackingGate@users.noreply.github.com>
2025-01-29 23:41:21 +0900

    Ran go run -mod=mod entgo.io/ent/cmd/ent new AltTube

---
+++ ent/schema/alttube.go
@@ -0,0 +1,18 @@
+package schema
+
+import "entgo.io/ent"
+
+// AltTube holds the schema definition for the AltTube entity.
+type AltTube struct {
+	ent.Schema
+}
+
+// Fields of the AltTube.
+func (AltTube) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the AltTube.
+func (AltTube) Edges() []ent.Edge {
+	return nil
+}
