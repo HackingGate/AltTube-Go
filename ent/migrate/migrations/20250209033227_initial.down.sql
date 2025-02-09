30186f0ff34a740647fd20a0777269fc54280381
HackingGate <8541644+HackingGate@users.noreply.github.com>
2025-02-09 12:33:18 +0900

    Create initial up and down

---
+++ ent/migrate/migrations/20250209033227_initial.down.sql
@@ -0,0 +1,10 @@
+-- Drop "access_tokens" table
+DROP TABLE "access_tokens";
+-- Drop "refresh_tokens" table
+DROP TABLE "refresh_tokens";
+-- Drop "like_videos" table
+DROP TABLE "like_videos";
+-- Drop "users" table
+DROP TABLE "users";
+-- Drop "videos" table
+DROP TABLE "videos";
